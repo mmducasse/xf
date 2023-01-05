d822d292226f242d92aaf335c34e4c01b09c95d0
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2023-01-04 18:23:40 -0800

    Added draw functions to Bitmap trait.

--- src/gl/bitmap.rs
+++ src/gl/bitmap.rs
@@ -1,10 +1,52 @@
-use crate::num::ivec2::IVec2;
+use crate::num::{ivec2::IVec2, irect::IRect};
 
-use super::color::Color;
+use super::{
+    color::Color, shader::DrawPixel, 
+    texture::Texture, 
+    draw_params::DrawParams
+};
 
 
 pub trait Bitmap {
     fn size(&self) -> IVec2;
     fn get_pixel(&self, pos: IVec2) -> Color;
     fn set_pixel(&mut self, pos: IVec2, color: Color);
+
+    fn draw_rect(&mut self, rect: IRect, color: Color) {
+        for pos in rect.iter() {
+            self.set_pixel(pos, color);
+        }
+    }
+
+    #[inline]
+    fn draw_texture_full(&mut self, texture: &Texture, dst_pt: IVec2) {
+        self.draw_texture(texture, texture.bounds(), dst_pt);
+    }
+
+    fn draw_texture(&mut self, texture: &Texture, src: IRect, dst_pt: IVec2) {
+        let params = DrawParams {
+            src: Some(src),
+            shader: None,
+        };
+
+        self.draw_texture_x(texture, dst_pt, params);
+    }
+
+    fn draw_texture_x(&mut self, texture: &Texture, dst_pt: IVec2, params: DrawParams) {
+        let src = params.src.unwrap_or(texture.bounds());
+
+        let offset = dst_pt - src.pos;
+
+        for src_pt in src.iter() {
+            let dst = src_pt + offset;
+            if let Some(&color) = texture.get(src_pt) {
+                let mut dp = DrawPixel { org: IVec2::ZERO, dst, color };
+                if let Some(shader) = &params.shader {
+                    dp = shader.apply(dp, texture);
+                }
+
+                self.set_pixel(dp.dst, dp.color);
+            }
+        }
+    }
 }
--- src/gl/draw.rs
+++ src/gl/draw.rs
@@ -1,70 +1,70 @@
-use crate::num::{ivec2::IVec2, irect::IRect};
+// use crate::num::{ivec2::IVec2, irect::IRect};
 
-use super::{
-    texture::Texture, 
-    color::Color, 
-    draw_params::DrawParams, 
-    shader::DrawPixel
-};
+// use super::{
+//     texture::Texture, 
+//     color::Color, 
+//     draw_params::DrawParams, 
+//     shader::DrawPixel
+// };
 
 
-static mut BUFFER: Option<Texture> = None;
+// static mut BUFFER: Option<Texture> = None;
 
-pub fn init(size: IVec2) {
-    unsafe {
-        BUFFER = Some(Texture::of_size(size));
-    }
-}
+// pub fn init(size: IVec2) {
+//     unsafe {
+//         BUFFER = Some(Texture::of_size(size));
+//     }
+// }
 
-pub fn get_buffer() -> &'static Option<Texture> {
-    unsafe { 
-        &BUFFER
-    }
-}
+// pub fn get_buffer() -> &'static Option<Texture> {
+//     unsafe { 
+//         &BUFFER
+//     }
+// }
 
-#[inline]
-pub fn draw_pixel(pt: IVec2, color: Color) {
-    unsafe {
-        if let Some(buffer) = &mut BUFFER {
-            buffer.set(pt, color);
-        }
-    }
-}
+// #[inline]
+// pub fn draw_pixel(pt: IVec2, color: Color) {
+//     unsafe {
+//         if let Some(buffer) = &mut BUFFER {
+//             buffer.set(pt, color);
+//         }
+//     }
+// }
 
-pub fn draw_rect(rect: IRect, color: Color) {
-    for pt in rect.iter() {
-        draw_pixel(pt, color);
-    }
-}
+// pub fn draw_rect(rect: IRect, color: Color) {
+//     for pt in rect.iter() {
+//         draw_pixel(pt, color);
+//     }
+// }
 
-#[inline]
-pub fn draw_texture_full(texture: &Texture, dst_pt: IVec2) {
-    draw_texture(texture, texture.bounds(), dst_pt);
-}
+// #[inline]
+// pub fn draw_texture_full(texture: &Texture, dst_pt: IVec2) {
+//     draw_texture(texture, texture.bounds(), dst_pt);
+// }
 
-pub fn draw_texture(texture: &Texture, src: IRect, dst_pt: IVec2) {
-    let params = DrawParams {
-        src: Some(src),
-        shader: None,
-    };
+// pub fn draw_texture(texture: &Texture, src: IRect, dst_pt: IVec2) {
+//     let params = DrawParams {
+//         src: Some(src),
+//         shader: None,
+//     };
 
-    draw_texture_x(texture, dst_pt, params);
-}
+//     draw_texture_x(texture, dst_pt, params);
+// }
 
-pub fn draw_texture_x(texture: &Texture, dst_pt: IVec2, params: DrawParams) {
-    let src = params.src.unwrap_or(texture.bounds());
+// pub fn draw_texture_x(texture: &Texture, dst_pt: IVec2, params: DrawParams) {
+//     let src = params.src.unwrap_or(texture.bounds());
 
-    let offset = dst_pt - src.pos;
+//     let offset = dst_pt - src.pos;
 
-    for src_pt in src.iter() {
-        let dst = src_pt + offset;
-        if let Some(&color) = texture.get(src_pt) {
-            let mut dp = DrawPixel { org: IVec2::ZERO, dst, color };
-            if let Some(shader) = &params.shader {
-                dp = shader.apply(dp, texture);
-            }
+//     for src_pt in src.iter() {
+//         let dst = src_pt + offset;
+//         if let Some(&color) = texture.get(src_pt) {
+//             let mut dp = DrawPixel { org: IVec2::ZERO, dst, color };
+//             if let Some(shader) = &params.shader {
+//                 dp = shader.apply(dp, texture);
+//             }
 
-            draw_pixel(dp.dst, dp.color);
-        }
-    }
-}
+//             draw_pixel(dp.dst, dp.color);
+//         }
+//     }
+// }
