131104de9f41b337a90473fbb7acd94180c03dea
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2023-01-21 13:03:47 -0800

    Added Countdown timer.

---
+++ src/time/countdown.rs
@@ -0,0 +1,23 @@
+
+
+pub struct Countdown {
+    rem: u32,
+}
+
+impl Countdown {
+    pub fn new(duration: u32) -> Self {
+        Self { rem: duration }
+    }
+
+    pub fn remaining(&self) -> u32 { self.rem }
+
+    pub fn decrement(&mut self) {
+        if self.rem > 0 {
+            self.rem -= 1;
+        }
+    }
+
+    pub fn is_done(&self) -> bool {
+        self.rem == 0
+    }
+}
