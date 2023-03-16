ffa431ed7d151f0af52ba379c6d738eb951ddb1d
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2023-03-15 17:49:39 -0700

    Simplified Animator.

--- src/anim/animator.rs
+++ src/anim/animator.rs
@@ -1,9 +1,11 @@
 use std::{hash::Hash, rc::Rc};
 
-use crate::num::{
+use macroquad::texture::Texture2D;
+
+use crate::{num::{
     irect::{ir, IRect},
     ivec2::IVec2,
-};
+}, mq::draw::draw_texture};
 
 use super::animation_map::AnimationMap;
 
@@ -13,30 +15,31 @@ pub struct Animator<T> {
     curr_time_s: f32,
     tile_size: IVec2,
     animations: Rc<AnimationMap<T>>,
+    texture: Texture2D,
 }
 
 impl<T> Animator<T>
 where
     T: Eq + Hash + Clone,
 {
-    pub fn new(start_key: T, tile_size: IVec2, animations: Rc<AnimationMap<T>>) -> Self {
+    pub fn new(start_key: T, tile_size: IVec2, animations: Rc<AnimationMap<T>>, texture: Texture2D) -> Self {
         Self {
             curr_key: start_key,
             curr_time_s: 0.0,
             tile_size,
             animations,
+            texture,
         }
     }
 
-    pub fn set_key(&mut self, key: T) {
-        self.curr_key = key;
-    }
-
-    pub fn update(&mut self, delta_s: f32) {
+    pub fn update(&mut self, delta_s: f32, new_key: Option<T>) {
+        if let Some(new_key) = new_key {
+            self.curr_key = new_key;
+        }
         self.curr_time_s += delta_s;
     }
 
-    pub fn curr_src_tile(&self) -> IRect {
+    fn curr_src_tile(&self) -> IRect {
         let Some(curr_animation) =
             self.animations
                 .get(self.curr_key.clone()) else {
@@ -45,5 +48,10 @@ where
 
         let src_tile = curr_animation.at(self.curr_time_s);
         ir(src_tile * self.tile_size, self.tile_size)
+    }
+
+    pub fn draw(&self, pos: IVec2) {
+        let src = self.curr_src_tile();
+        draw_texture(self.texture, Some(src), pos)
     }
 }
