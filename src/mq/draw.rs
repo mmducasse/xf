965f3bb337985c45839f29337ae8e0178a4d66fe
Matthew Maurice Ducasse <matthewmducasse@gmail.com>
2024-10-12 18:53:21 -0700

    Added draw_ellipse function.

--- src/mq/draw.rs
+++ src/mq/draw.rs
@@ -1,6 +1,5 @@
 use macroquad::{
     prelude::{Color, WHITE},
-    shapes::draw_rectangle,
     texture::{draw_texture_ex, DrawTextureParams},
 };
 
@@ -9,13 +8,17 @@ use crate::num::{irect::IRect, ivec2::IVec2};
 use super::texture::Texture;
 
 pub fn draw_rect(rect: IRect, color: Color) {
-    draw_rectangle(
+    macroquad::shapes::draw_rectangle(
         rect.x() as f32,
         rect.y() as f32,
         rect.w() as f32,
         rect.h() as f32,
         color,
     );
+}
+
+pub fn draw_ellipse(center: IVec2, size: IVec2, color: Color) {
+    macroquad::shapes::draw_ellipse(center.x as f32, center.y as f32, size.x as f32, size.y as f32, 0.0, color);
 }
 
 pub fn draw_texture(texture: Texture, src: Option<IRect>, dst: IVec2) {
